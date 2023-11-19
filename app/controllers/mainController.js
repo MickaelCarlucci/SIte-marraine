057d202cdf086bae761c49a01eff7eb4ec2627ec
MickaelCarlucci <frewmike17@gmail.com>
2023-11-19 19:58:02 +0100

    Route de 1 tableau effectué, ejs a faire

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,16 +1,16 @@
 const arrays = require('../../tableaux.json')
 
 const mainController = {
+
+    //page d'accueil avec 2 tableaux qui se mettent aléatoirement TODO
     homePage: (request, response) => {
 
         response.render('home.ejs')
     },
 
+    //page de tous les tableaux avec boucle dans la page ejs a partir du json
     arrays: (request, response) => {
-        const allArrays = arrays;
-
-            console.log(allArrays)
-        
+        const allArrays = arrays;  
 
         response.render('arrays.ejs', {allArrays})
     },
@@ -21,9 +21,14 @@ const mainController = {
         response.render('contact.ejs')
     },
 
+    //page qui affiche un tableau
     array: (request, response) => {
-        
-        response.render('array.ejs')
+        //on récupère l'id de la page
+        const id = Number(request.params.id);
+        // on l'envoie sur l'array qui correspond et on récupere le tableau
+        const oneArray = arrays.find((array) => array.id === id)
+        console.log(oneArray)
+        response.render('array.ejs', {oneArray})
     }
 
 }
