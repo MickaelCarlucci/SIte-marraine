9f0d28361b62ecc1a9dce021bfc495b29d5cf3c3
MickaelCarlucci <frewmike17@gmail.com>
2023-11-25 00:08:59 +0100

    Dynamisation de la page d'accueil

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -2,17 +2,29 @@ const arrays = require('../../tableaux.json')
 
 const mainController = {
 
-    //page d'accueil avec 2 tableaux qui se mettent aléatoirement TODO
+    //page d'accueil avec 2 tableaux qui se mettent aléatoirement TODO 
     homePage: (request, response) => {
-
-        response.render('home.ejs')
+        const urlArrays = arrays.map(array => array.url_img);
+       // const randomUrl = urlArrays[Math.floor(Math.random() * urlArrays.length)];
+    
+        function getRandomUrl() {
+            const randomIndex = Math.floor(Math.random() * urlArrays.length);
+            return urlArrays[randomIndex];
+        }
+    
+            const randomUrl = getRandomUrl();
+            
+    
+        response.render('home.ejs', { randomUrl,  getRandomUrl });
     },
+    
+    
 
     //page de tous les tableaux avec boucle dans la page ejs a partir du json
     arrays: (request, response) => {
         const allArrays = arrays;  
 
-        response.render('arrays.ejs', {allArrays})
+        response.render('arrays.ejs', { allArrays })
     },
 
     contact: (request, response) => {
