0c4061c9afbb57d8a74fe4ee8c368d50b03c01d6
MickaelCarlucci <frewmike17@gmail.com>
2023-12-26 10:48:58 +0100

    Ajout du seed

---
+++ data/seed_database.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+INSERT INTO "paintings"
+("id", "url_img", "description")
+VALUES
+(1,'https://img.freepik.com/photos-gratuite/abstrait-colore-splash-3d-fond-generatif-fond-ai_60438-2509.jpg?size=626&ext=jpg', 'Jolie peinture' ),
+(2, 'https://img.freepik.com/photos-premium/peinture-deux-femmes-dans-champ-fleurs-parapluie-ai-generatif_958192-1727.jpg?size=626&ext=jpg', 'Incroyable'),
+(3, 'https://img.freepik.com/photos-gratuite/texture-acrylique-abstraite_23-2148132139.jpg?size=626&ext=jpg', 'Superbe'),
+(4, 'https://img.freepik.com/photos-premium/peinture-abstraite-aux-couleurs-jaune-noir-blanc_893012-93228.jpg?size=626&ext=jpg', 'Amazing !'),
+(5, 'https://img.freepik.com/photos-premium/peinture-femme-aux-cheveux-colores_893012-85987.jpg?size=626&ext=jpg&ga=GA1.1.657728215.1700411191&semt=sph', 'Jolie peinture !!');
+
+
+SELECT setval('paintings', (SELECT MAX(id) from "paintings"));
+
+COMMIT;
