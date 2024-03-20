fc23b1cf67213a1b16df3b2a3652feeed517a0f6
Mike pc portable <frewmike17@gmail.com>
2024-03-20 16:48:58 +0100

    correction des images + ajout dans le seeding

--- data/seed_database.sql
+++ data/seed_database.sql
@@ -1,13 +1,54 @@
 BEGIN;
 
 INSERT INTO "paintings"
-("id", "url_img", "description")
+("id", "url_img", "title", "description", "price")
 VALUES
-(1,'https://img.freepik.com/photos-gratuite/abstrait-colore-splash-3d-fond-generatif-fond-ai_60438-2509.jpg?size=626&ext=jpg', 'Jolie peinture' ),
-(2, 'https://img.freepik.com/photos-premium/peinture-deux-femmes-dans-champ-fleurs-parapluie-ai-generatif_958192-1727.jpg?size=626&ext=jpg', 'Incroyable'),
-(3, 'https://img.freepik.com/photos-gratuite/texture-acrylique-abstraite_23-2148132139.jpg?size=626&ext=jpg', 'Superbe'),
-(4, 'https://img.freepik.com/photos-premium/peinture-abstraite-aux-couleurs-jaune-noir-blanc_893012-93228.jpg?size=626&ext=jpg', 'Amazing !'),
-(5, 'https://img.freepik.com/photos-premium/peinture-femme-aux-cheveux-colores_893012-85987.jpg?size=626&ext=jpg&ga=GA1.1.657728215.1700411191&semt=sph', 'Jolie peinture !!');
+(1,'/painting/tango_torride.jpg', 'Tango Torride', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(2,'/painting/a_fond_les_volants.jpg', 'A fond les volants', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(3,'/painting/akhenaton.jpg', 'Akhenaton', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(4,'/painting/arlequin.jpg', 'Arlequin', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(5,'/painting/arlequine.jpg', 'Arlequine', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(6,'/painting/ballade_en_deux_chevaux.jpg', 'Ballade en 2 cheveaux', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(7,'/painting/bateau_a_voile_rouge.jpg', 'Bateau à voile rouge', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(8,'/painting/bateaux_dans_l_horizon_lointain.jpg', "Bateaux dans l'horizon", 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(9,'/painting/bouddha.jpg', 'Bouddha', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(10,'/painting/bouquet_a_double_face.jpg', 'Le bouquet à double face', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(11,'/painting/bouquet_de_fleurs_pastel.jpg', 'Les fleurs pastel', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(12,'/painting/brave_toutou.jpg', 'Gentil toutou', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(13,'/painting/composition_florale.jpg', 'Composition florale', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(14,'/painting/danseuse_de_flamenco.jpg', 'Flamenco', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(15,'/painting/deux_cheveaux_rigolote.jpg', 'La rigolote', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(16,'/painting/fleur_blanche_sur_fond_rouge.jpg', 'Les fleurs blanches du rouge', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(17,'/painting/fleur_blanche.jpg', 'Les blanches', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(18,'/painting/freddy_mercury.jpg', 'Freddy', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(19,'/painting/grand_chef.jpg', 'Grand chef', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(20,'/painting/jeune_fille_d_automne.jpg', "Jeune fille de l'automne", 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(21,'/painting/jeux_d_enfant.png', 'Jeux enfantin', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(22,'/painting/johny_halliday.jpg', 'Johny', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(23,'/painting/jolie_parisienne.jpg', 'La jolie Parisienne', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(24,'/painting/l_amour_d_un_chat.jpg', 'Perle', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(25,'/painting/la_barque.jpg', 'La barque', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(26,'/painting/le_pecheur_solitaire.jpg', 'Le solitaire', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(27,'/painting/les_femmes_s_eloignant.jpg', 'Parle sur nous', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(28,'/painting/lion.jpg', 'Lion', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(29,'/painting/mecanique_du_futur.jpg', 'Mecanique du futur', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(30,'/painting/panorama_paradisiaque.jpg', 'Paradisiaque', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(31,'/painting/papillons.jpg', 'Papillons', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(32,'/painting/parasol_bleu.jpg', 'Le parasol', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(33,'/painting/paysage_de_montagne.jpg', 'La montagne', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(34,'/painting/paysage_de_printemps.jpg', 'Le printemps', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(35,'/painting/pecheurs.jpg', 'Les pecheurs', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(36,'/painting/petits_bateaux.jpg', 'Petits bateaux', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(37,'/painting/princesse_dorée.jpg', 'La princesse dorée', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(38,'/painting/red.jpg', 'Red', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(39,'/painting/reflet_city.jpg', 'La ville des reflets', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(40,'/painting/riziere.png', 'La rizière', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(41,'/painting/soiree_entre_dame.jpg', 'Entre nous', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(42,'/painting/travail_dans_les_champs.png', 'Le labeur', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(43,'/painting/une_mere_et_son_enfant.jpg', 'Le lien', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(44,'/painting/village_en_bord_de_mer.jpg', 'Au bord de la mer', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(45,'/painting/voilier_solitaire.jpg', 'Le voilier', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 ),
+(46,'/painting/yorkshire.jpg', 'Le york', 'cette peinture représente deux danseurs de tango enlacés qui se regarde yeux dans les yeux', 50 );
 
 SELECT setval('paintings_id_seq', (SELECT MAX(id) FROM "paintings"));
 
